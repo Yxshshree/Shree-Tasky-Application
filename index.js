ef9148d93f2b85665ccee1b3b39d749ab7041c9a
yxshshree-github <gokartechill@gmail.com>
2024-06-26 18:23:07 +0530

    Changes done in  3 files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("connected..");
+const state = {
+  taskList: [],
+};
+
+// DOM Operations
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+// console.log(taskContents);
+// console.log(taskModal);
+
+const htmlTaskContent = ({ id, title, description, type, url }) => {};
